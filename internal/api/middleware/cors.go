adfe87fb0a4f164a1236f3482e941dfd6687ca4b
王九日 <wangxucs123@126.com>
2025-07-28 17:29:42 +0800

    fix: cors support other header

--- internal/api/middleware/cors.go
+++ internal/api/middleware/cors.go
@@ -10,7 +10,7 @@ func CorsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, Bucket_name,Region_code,x-upload-task-id,upload-task-id,x-file-name,")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
 		if c.Request.Method == "OPTIONS" {
